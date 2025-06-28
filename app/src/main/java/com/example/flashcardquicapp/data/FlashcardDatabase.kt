dbbbf3c909e55338ac88374b84f7bae6e0b0ce76
WildWatch <akalive12356@gmail.com>
2025-06-28 17:19:58 +0500

    Initial commit for CodeAlpha Flashcard Quiz App

---
+++ app/src/main/java/com/example/flashcardquicapp/data/FlashcardDatabase.kt
@@ -0,0 +1,21 @@
+package com.example.flashcardquicapp.data
+
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FlashcardDao {
+
+    @Query("SELECT * FROM flashcards")
+    fun getAllFlashcards(): Flow<List<Flashcard>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFlashcard(flashcard: Flashcard)
+
+    @Update
+    suspend fun updateFlashcard(flashcard: Flashcard)
+
+    @Delete
+    suspend fun deleteFlashcard(flashcard: Flashcard)
+}
